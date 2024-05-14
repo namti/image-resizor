312ab258d522cbd85ee350b68cc1fef3f14944d0
Jasper Yan <jasper@inboxdesign.co.nz>
2024-05-14 13:18:01 +1200

    Remove cjs format

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
-import { resolve } from 'pathe'
+import { resolve } from 'pathe';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  plugins: [ dts({
-    insertTypesEntry: true,
-  }) ],
+  plugins: [ dts({ insertTypesEntry: true }) ],
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
@@ -13,7 +11,11 @@ export default defineConfig({
       name: 'ImageResizor',
       // the proper extensions will be added
       fileName: 'image-resizor',
-      formats: ['cjs', 'umd', 'iife', 'es'],
+      formats: [
+        'umd',
+        'iife',
+        'es',
+      ],
     },
   },
-})
+});
